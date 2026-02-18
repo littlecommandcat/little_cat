67341f1dc289e2c75b249e09190727265b2938e9
littlecommandcat <cyingda742@gmail.com>
2026-02-18 17:27:29 +0800

    Update RPG and Fakebot document.

--- script/redirect.js
+++ script/redirect.js
@@ -10,8 +10,8 @@ document.querySelectorAll('section').forEach(section => {
     observer.observe(section);
 });
 
-const buttons = document.querySelectorAll('#inviteBtn, #inviteBtnBottom');
-buttons.forEach(btn => {
+const invitebuttons = document.querySelectorAll('#inviteBtn, #inviteBtnBottom');
+invitebuttons.forEach(btn => {
     btn.addEventListener('click', () => {
         document.body.classList.add('fade-out-all');
         const overlay = document.createElement('div');
@@ -20,6 +20,20 @@ buttons.forEach(btn => {
 
         setTimeout(() => {
             window.location.href = "https://discord.com/oauth2/authorize?client_id=1144161789832069141";
+        }, 1000);
+    });
+});
+
+const nyankobutton = document.querySelectorAll('#nyankohost');
+nyankobutton.forEach(btn => {
+    btn.addEventListener('click', () => {
+        document.body.classList.add('fade-out-all');
+        const overlay = document.createElement('div');
+        overlay.className = 'blue-overlay';
+        document.body.appendChild(overlay);
+
+        setTimeout(() => {
+            window.location.href = "https://dctw.xyz/servers/1404587685645123665";
         }, 1000);
     });
 });
